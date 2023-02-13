3dac767b1fe65035bf21af61d980ac9bb06a61a9
nguyenvu <nguyenvu.hcmus@gmail.com>
2023-02-13 17:22:13 +0700

    connect to database

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
+require('dotenv').config()
 const createError = require('http-errors')
 const express = require('express')
-const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
-const knex = require('knex')
-
-const indexRouter = require('./routes/index')
 
 const app = express()
 
@@ -13,9 +10,8 @@ app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
-app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', indexRouter)
+app.use('/', require('./routes/index'))
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
---
+++ database/database.js
@@ -0,0 +1,23 @@
+
+require('dotenv').config()
+
+const knex = require('knex')({
+  client: 'mysql',
+  connection: {
+    host: process.env.DB_HOST || '127.0.0.1',
+    port: process.env.DB_PORT || 3306,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+  },
+  pool: { min: 0, max: 7 }
+})
+
+knex.raw('SELECT VERSION()').then(() => {
+  console.log('connect to database successfully')
+})
+  .catch(err => {
+    console.log(err)
+  })
+
+module.exports = knex
